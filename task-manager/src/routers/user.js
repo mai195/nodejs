bc21ab0babd2bb00bd5e9d15af09056a05e79134
mai <tthm280@gmail.com>
2023-10-02 10:12:14 -0400

    refactoring and formatting images

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const multer = require('multer')
+const sharp = require('sharp')
 const User = require('../models/user')
 const Task = require('../models/task')
 const auth = require('../middleware/auth')
@@ -122,7 +123,8 @@ const upload = multer({
 })
 
 router.post('/users/me/avatar', auth, upload.single('avatar'), async(req, res) => {
-    req.user.avatar = req.file.buffer 
+    const buffer = await sharp(req.file.buffer).resize({width: 250, height: 250}).png().toBuffer()
+    req.user.avatar = buffer
     await req.user.save()
     res.send()
 }, (error, req, res, next) => {
@@ -133,6 +135,21 @@ router.delete('/users/me/avatar', auth, async(req, res) => {
     req.user.avatar = undefined
     await req.user.save()
     res.send()
+})
+
+router.get('/users/:id/avatar', async(req, res) => {
+    try{
+        const user = await User.findById(req.params.id)
+
+        if(!user || !user.avatar){
+            throw new Error()
+        }
+
+        res.set('Content-Type', 'image/png')
+        res.send(user.avatar)
+    }catch(e){
+        res.status(404).send()
+    }
 })
 
 module.exports = router
