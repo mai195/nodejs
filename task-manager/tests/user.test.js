2372f5d69a70ab260a06bc4380cae84b2cb1dc25
mai <tthm280@gmail.com>
2023-10-13 09:41:51 -0400

    setup before and after

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -1,5 +1,17 @@
 const request = require('supertest')
 const app = require('../src/app')
+const User = require('../src/models/user')
+
+const userOne = {
+    name: "Mai 1",
+    email: "mai1@mydomain.com",
+    password: "1234567"
+}
+
+beforeEach(async() => {
+    await User.deleteMany()
+    await new User(userOne).save()
+})
 
 test('Should sign up a new user', async() => {
     await request(app).post('/users').send({
@@ -7,4 +19,18 @@ test('Should sign up a new user', async() => {
         email: 'mai4@mydomain.com',
         password: '1234567'
     }).expect(201)
+})
+
+test('Should log in existing user', async() => {
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: userOne.password
+    }).expect(200)
+})
+
+test('Should not login nonexistent user', async() => {
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: "wrongpassword"
+    }).expect(400)
 })
